ded8e4a810a85ed4e0ce138b837ff4cc7e08943c
Harris Wan <117702329+harris-wan-analyst@users.noreply.github.com>
2022-12-25 19:34:13 -0500

    Update customer_behavior.sql

--- customer_behavior.sql
+++ customer_behavior.sql
@@ -5,7 +5,7 @@
 
 SELECT 
 	COUNT(DISTINCT user_id) user_count,
-	COUNT(DISTINCT product_id) order_count,
+	COUNT(DISTINCT product_id) prod_count,
 	COUNT(DISTINCT aisle_id) aisle_count,
 	COUNT(DISTINCT department_id) department_count
 FROM instacart_mdl.orders;
